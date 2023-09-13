c9dfe75f2aa0ea7aaa90b73e8437ac3e3c34172b
znat <nathan.zylbersztejn@gmail.com>
2023-09-13 19:15:57 +0200

    dynamic gql uri

--- app/AppWrapper.tsx
+++ app/AppWrapper.tsx
@@ -1,52 +1,61 @@
-"use client";
+'use client';
 // ^ this file needs the "use client" pragma
 
-import { ApolloLink, HttpLink } from "@apollo/client";
+import { ApolloLink, HttpLink } from '@apollo/client';
 import {
-  ApolloNextAppProvider,
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-  SSRMultipartLink,
-} from "@apollo/experimental-nextjs-app-support/ssr";
+    ApolloNextAppProvider,
+    NextSSRInMemoryCache,
+    NextSSRApolloClient,
+    SSRMultipartLink,
+} from '@apollo/experimental-nextjs-app-support/ssr';
 
-const uri = process.env.NEXT_PUBLIC_VERCEL_URL? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql` : "http://localhost:3000/api/graphql";
+const uri = () => {
+    if (process.env.VERCEL_ENV === 'preview') {
+        return `https://${process.env.VERCEL_BRANCH_URL}/api/graphql`;
+    } else if (process.env.VERCEL_ENV === 'production') {
+        return `https://${process.env.VERCEL_URL}/api/graphql`;
+    } else {
+        return 'http://localhost:3000/api/graphql';
+    }
+};
+
 // have a function to create a client for you
 function makeClient() {
-  const httpLink = new HttpLink({
-    // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri,
-    // you can disable result caching here if you want to
-    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-    fetchOptions: { cache: "no-store" },
-    // you can override the default `fetchOptions` on a per query basis
-    // via the `context` property on the options passed as a second argument
-    // to an Apollo Client data fetching hook, e.g.:
-    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
-  });
+    const httpLink = new HttpLink({
+        // this needs to be an absolute url, as relative urls cannot be used in SSR
+        uri,
+        // you can disable result caching here if you want to
+        // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+        fetchOptions: { cache: 'no-store' },
+        // you can override the default `fetchOptions` on a per query basis
+        // via the `context` property on the options passed as a second argument
+        // to an Apollo Client data fetching hook, e.g.:
+        // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
+    });
 
-  return new NextSSRApolloClient({
-    // use the `NextSSRInMemoryCache`, not the normal `InMemoryCache`
-    cache: new NextSSRInMemoryCache(),
-    link:
-      typeof window === "undefined"
-        ? ApolloLink.from([
-            // in a SSR environment, if you use multipart features like
-            // @defer, you need to decide how to handle these.
-            // This strips all interfaces with a `@defer` directive from your queries.
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            httpLink,
-          ])
-        : httpLink,
-  });
+    return new NextSSRApolloClient({
+        // use the `NextSSRInMemoryCache`, not the normal `InMemoryCache`
+        cache: new NextSSRInMemoryCache(),
+        link:
+            typeof window === 'undefined'
+                ? ApolloLink.from([
+                      // in a SSR environment, if you use multipart features like
+                      // @defer, you need to decide how to handle these.
+                      // This strips all interfaces with a `@defer` directive from your queries.
+                      new SSRMultipartLink({
+                          stripDefer: true,
+                      }),
+                      httpLink,
+                  ])
+                : httpLink,
+    });
 }
 
 // you need to create a component to wrap your app in
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
-  return (
-    <ApolloNextAppProvider makeClient={makeClient}>
-      {children}
-    </ApolloNextAppProvider>
-  );
+    return (
+        <ApolloNextAppProvider makeClient={makeClient}>
+            {children}
+        </ApolloNextAppProvider>
+    );
 }
